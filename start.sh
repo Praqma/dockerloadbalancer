26ce364fa82ec34a39f9363831fdf0271cf389f7
hoeghh <heh@praqma.net>
2016-01-07 19:28:04 +0100

    A better example

--- start.sh
+++ start.sh
@@ -1,11 +1,24 @@
+# First we build our images
+docker-compose build
+
+# Then we scale our app to our startingport size
+docker-compose scale app=2
+
+# Now we can startup our application
 docker-compose up -d
+
+# Display four page loads
 sleep 2
 curl localhost
 curl localhost
 curl localhost
+curl localhost
 
+# Scaling to 4 apps
 docker-compose scale app=4
-docker-compose up --force-recreate -d
+docker-compose up  --force-recreate -d
+
+# Display four page loads
 sleep 2
 curl localhost
 curl localhost
